28c1e6a23d59bec800b6ed15822dce839b32ba9c
miguel26697 <miguelrippe01@gmail.com>
2022-03-06 12:38:25 -0500

    arreglo mapper1.py

--- bigdata/corte1/map-reduce/code/punto1/mapper1.py
+++ bigdata/corte1/map-reduce/code/punto1/mapper1.py
@@ -1,16 +1,11 @@
 import sys
 
-fechas = []
+
 for line in sys.stdin:
         datos = line.split(",")
-        fechas.append(datos[2])
-
-fechas.pop(0)
-
-años = []
-for date in fechas:
-        dates = date.split("-")
-        años.append(dates[0])
-
-for año in años:
-         print (str(año) + "\t" + str(1))
+        fechas = None
+        if datos[2] != "Date of Transfer": 
+                fechas = datos[2]
+        if fechas != None:
+         años = fechas.split("-")
+         print (str(años[0]) + "\t" + str(1))
