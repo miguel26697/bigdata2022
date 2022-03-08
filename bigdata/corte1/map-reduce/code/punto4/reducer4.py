930ea6c328c1824ff2ebf72708f5d5873f86f235
miguel26697 <miguelrippe01@gmail.com>
2022-03-08 18:59:05 -0500

    cambios

--- bigdata/corte1/map-reduce/code/punto4/reducer4.py
+++ bigdata/corte1/map-reduce/code/punto4/reducer4.py
@@ -7,6 +7,5 @@ for line in sys.stdin:
         key, value, precio, año = line.split("\t")
         value = int(value)
         precio = int(precio)
-        current_precio.append(precio)
-ordenados = sorted(current_precio)
-print (str(key) + "\t" + str(año) + "\t" + str(ordenados))
+        print (str(key) + "\t" + str(precio) + "\t" + str(año))
+
