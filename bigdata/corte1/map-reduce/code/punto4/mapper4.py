6a8b5091dc87c08ed924b1af533094c4534960a1
miguel26697 <miguelrippe01@gmail.com>
2022-03-09 00:02:05 -0500

    correciones

--- bigdata/corte1/map-reduce/code/punto4/mapper4.py
+++ bigdata/corte1/map-reduce/code/punto4/mapper4.py
@@ -2,19 +2,18 @@ import sys
 
 
 for line in sys.stdin:
-        datos = line.split(",")
-        precios = None
-        ciudades = None
-        fechas = None
-        
-        if datos[6] != "Town/City":
-                ciudades = datos[6]
-        if datos[2] != "Date of Transfer":
-                fechas = datos[2]
+    datos = line.split(",")
+    precios = None
+    ciudades = None
+    fechas = None
+    if datos[1] != "Price":
+        precios = datos[1]
+    if datos[6] != "Town/City":
+        ciudades = datos[6]
+    if datos[2] != "Date of Transfer":
+        fechas = datos[2]
 
-        if ciudades != None and fechas != None:
-                años = fechas.split("-")
-                if años [0] == "2015" and ciudades == "STAMFORD":
-                        if datos[1] != "Price":
-                                precios = datos[1]
-                                print (str(precios) + "\t"+ str(1) + "\t" + str(ciudades) + "\t" + str(años[0]))
+    if ciudades != None and fechas != None and precios != None:
+        años = fechas.split("-")
+        if años[0] == "2015" and ciudades == "STAMFORD":
+            print(str(precios) + "\t" + str(1) + "\t" + str(ciudades) + "\t" + str(años[0]))
