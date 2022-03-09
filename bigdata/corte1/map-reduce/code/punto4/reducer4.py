f1469ce0af4bb2ee90f5f52565f5880c7c437120
miguel26697 <miguelrippe01@gmail.com>
2022-03-08 23:41:35 -0500

    correciones

--- bigdata/corte1/map-reduce/code/punto4/reducer4.py
+++ bigdata/corte1/map-reduce/code/punto4/reducer4.py
@@ -1,14 +1,12 @@
 import sys
 
 current_key = None
-current_precio = []
+
 
 for line in sys.stdin:
     precios, value, key, año = line.split("\t")
     value = int(value)
     precio = int(precio)
-    current_precio.append(precio)
+    print(str("STAMFORD")+"\t"+str("2015")+"\t"+str(precio))
 
 
-for i in sorted(current_precio):
-    print(str("STAMFORD")+"\t"+str("2015")+"\t"+str(i))
