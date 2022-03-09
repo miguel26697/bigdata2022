f411841c9ab5ca31e038de1416d6a70609b022f5
miguel26697 <miguelrippe01@gmail.com>
2022-03-08 22:54:04 -0500

    correciones

--- bigdata/corte1/map-reduce/code/punto4/mapper4.py
+++ bigdata/corte1/map-reduce/code/punto4/mapper4.py
@@ -17,4 +17,5 @@ for line in sys.stdin:
                 if años [0] == "2015" and ciudades == "STAMFORD":
                         if datos[1] != "Price":
                                 precios = datos[1]
+                                precios = int(precios)
                                 print (str(precios) + "\t"+ str(1) + "\t" + str(ciudades) + "\t" + str(años[0]))
