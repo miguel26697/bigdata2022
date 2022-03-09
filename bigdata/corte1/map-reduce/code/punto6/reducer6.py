12139912ccf0a6b83e1598d99c6d5c77290f9024
Miguel Rippe <miguelrippe01@gmail.com>
2022-03-09 12:49:47 -0500

    cambios

--- bigdata/corte1/map-reduce/code/punto6/reducer6.py
+++ bigdata/corte1/map-reduce/code/punto6/reducer6.py
@@ -2,14 +2,17 @@ import sys
 
 current_key = None
 current_value = 0
+current_town = None
 for line in sys.stdin:
         condado, ciudad, value = line.split("\t")
         value = int(value)
         if current_key == None:
           current_key = condado
+          current_town = ciudad
 
         if current_key == condado:
-          current_value += value
+          if current_town != ciudad:
+              current_value += value
 
         else:
            print (str(current_key) + "\t" + str(current_value))
