809a459dd20c8cd2d9733ee9f2c001977a64afde
miguel26697 <miguelrippe01@gmail.com>
2022-04-28 15:18:36 -0500

    correciones

--- parcial2/lambda1/app.py
+++ parcial2/lambda1/app.py
@@ -3,38 +3,29 @@ import requests
 import time
 
 def handler(event, context):
-    localtime=time.localtime()
+    
     rBBC = requests.get('https://www.bbc.com/mundo')
     rCNN = requests.get('https://cnnespanol.cnn.com/')
     
+    put(rBBC,"BBC")
+    put(rCNN,"CNN")
     
-    print("Creating temporaly file...")
-    fileBBC="/tmp/BBC.html"
-    f = open(fileBBC,"w")
-    print("Saving file from BBC")
-    f.write(rBBC.text)
-    f.close()
 
+def put(r,periodico):
+    localtime=time.localtime()
     print("Creating temporaly file...")
-    fileCNN="/tmp/CNN.html"
-    f = open(fileCNN,"w")
+    file="/tmp/pagina.html"
+    f = open(file,"w")
     print("Saving file from BBC")
-    f.write(rCNN.text)
+    f.write(r.text)
     f.close()
 
     s3 = boto3.resource('s3')
     data={
-        'file':fileBBC,
+        'file':file,
         'bucket': 'scrapingnewspaper',
-        'path':('headlines/raw/periodico=BBC/year='+str(localtime.tm_year)+'/month='+str(localtime.tm_mon)+'/day='+str(localtime.tm_mday)+'/pagina.html')
+        'path':('headlines/raw/periodico='+periodico+'/year='+str(localtime.tm_year)+'/month='+str(localtime.tm_mon)+'/day='+str(localtime.tm_mday)+'/pagina.html')
     }
 
-    s3.meta.client.upload_file(data['file'],data['bucket'] , data['path'])
+    s3.meta.client.upload_file(data['file'],data['bucket'] ,data['path'])
 
-    data2={
-        'file':fileCNN,
-        'bucket': 'scrapingnewspaper',
-        'path':('headlines/raw/periodico=CNN/year='+str(localtime.tm_year)+'/month='+str(localtime.tm_mon)+'/day='+str(localtime.tm_mday)+'/pagina.html')
-    }
-
-    s3.meta.client.upload_file(data2['file'],data2['bucket'] ,data2['path'])
