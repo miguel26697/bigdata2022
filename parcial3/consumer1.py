88a924b3943d9cc3cb5970466f6cb643be665517
miguel26697 <miguelrippe01@gmail.com>
2022-05-30 22:42:44 -0500

    correciones

--- parcial3/consumer1.py
+++ parcial3/consumer1.py
@@ -1,6 +1,22 @@
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1'
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
+from pyspark.sql.functions import when
+import argparse
+from pyspark.sql.types import IntegerType
 
+
+parser =argparse.ArgumentParser()
+parser.add_argument("--maximo",help ="maximo valor: ")
+parser.add_argument("--minimo",help ="minimo valor: ")
+arg = parser.parse_args()
+
+if arg.maximo:
+     maximo = arg.maximo
+  
+if arg.minimo:
+     minimo= arg.minimo
 
 spark = SparkSession \
     .builder \
@@ -14,18 +30,25 @@ lines = spark \
     .option("subscribe","quickstart-events") \
     .option("batchDuration","10")\
     .load()
+
 df=lines.withColumn("Price", f.conv(f.col("value"), 16, 16).cast("bigint"))
 
 
 df2 = df.select("Price")
-dfPrice=df2.select(f.min("Price").alias("PrecioMin"),
-                    f.max("Price").alias("PrecioMax"))
+dfPrice=df2.select(f.min("Price").alias("minPrice").cast(IntegerType()),
+                    f.max("Price").alias("maxPrice").cast(IntegerType()))
+
+dfFinal1=dfPrice.withColumn("alertaMinimo", when(dfPrice.minPrice < minimo,"Debajo del minimo").otherwise("> minimo"))
+dfFinal = dfFinal1.withColumn("alertaMaximo", when(dfPrice.maxPrice > maximo,"Arriba del maximo").otherwise("< maximo"))
 
 
-query = dfPrice \
+
+query = dfFinal \
     .writeStream \
     .outputMode("complete") \
     .format("console") \
     .start()
+
+query.awaitTermination()
 
 query.awaitTermination()
